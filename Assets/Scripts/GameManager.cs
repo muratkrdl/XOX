b95fe07d57c9cfcf6f3d0fb7efa70564a364564c
muratkrdl <muratkirdal0101@gmail.com>
2023-10-18 01:22:40 +0300

    Project added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{   
+    public static GameManager instance { get; private set; }
+
+    public int turn { get; set; }
+
+    public List<TileController> ListTileController => listTileController;
+    [SerializeField] List<TileController> listTileController;
+    [SerializeField] Canvas canvas;
+
+    void Awake() 
+    {
+        instance = this;   
+    }
+
+    public (bool, TileState) HasWinner()
+    {
+        foreach(var tile in listTileController)
+        {
+            if(tile.MyState == TileState.None) continue;
+
+            foreach(var direction in Enum.GetValues(typeof(Direction)))
+            {
+                var nextTile = tile.GetNextTile((Direction)direction);
+                if(!nextTile) continue;
+
+                if(tile.MyState != nextTile.MyState) continue;
+
+                var lastTile = nextTile.GetNextTile((Direction)direction);
+                if(!lastTile) continue;
+                if(lastTile.MyState != tile.MyState) continue;
+
+                return (true, tile.MyState);
+            }
+        }
+
+        return (false, TileState.None);
+    }
+
+}
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum TileState
+{
+    None,
+    X,
+    O
+}
+
+public enum Direction
+{
+    Up,
+    UpRight,
+    Right,
+    DownRight,
+    Down,
+    LeftDown,
+    Left,
+    UpLeft
+}
+
+
+public class TileController : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] SpriteRenderer mySpriteRenderer;
+    
+    [SerializeField] Sprite xSprite;
+    [SerializeField] Sprite oSprite;
+
+    [SerializeField] Color xColor;
+    [SerializeField] Color oColor;
+
+
+    public TileState MyState { get; set; }
+
+    public Vector2 coordinate;
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if(MyState != TileState.None) { return; }
+
+        var state = GameManager.instance.turn % 2 == 0 ? TileState.X : TileState.O;
+        SetState(state);
+        GameManager.instance.turn++;
+
+        var result = GameManager.instance.HasWinner();
+        if(result.Item1)
+        {
+            Debug.Log($"Winner is: {result.Item2}");
+        }
+    }
+
+    public void SetState(TileState state)
+    {
+        MyState = state;
+        mySpriteRenderer.color = state == TileState.X ? xColor : oColor;
+        mySpriteRenderer.sprite = state == TileState.X ? xSprite : oSprite;
+    }
+
+    public TileController GetNextTile(Direction direction)
+    {
+        var nextTileCoordinate = coordinate;
+        switch(direction)
+        {
+            case Direction.Up:
+                nextTileCoordinate.y++;
+                break;
+            case Direction.UpRight:
+                nextTileCoordinate.y++;
+                nextTileCoordinate.x++;
+                break;
+            case Direction.Right:
+                nextTileCoordinate.x++;
+                break;
+            case Direction.DownRight:
+                nextTileCoordinate.x++;
+                nextTileCoordinate.y--;
+                break;
+            case Direction.Down:
+                nextTileCoordinate.y--;
+                break;
+            case Direction.LeftDown:
+                nextTileCoordinate.y--;
+                nextTileCoordinate.x--;
+                break;
+            case Direction.Left:
+                nextTileCoordinate.x--;
+                break;
+            case Direction.UpLeft:
+                nextTileCoordinate.x--;
+                nextTileCoordinate.y++;
+                break;
+        } 
+
+        return GameManager.instance.ListTileController.Find(tile => tile.coordinate == nextTileCoordinate);
+    }
+
+}
